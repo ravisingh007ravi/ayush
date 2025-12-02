a845ab4b70bc942c1e7966e9d0e7050a775f9e0e
Ravi Singh <ravisingh007ravi@gmail.com>
2025-12-02 14:08:18 +0530

    sdfsad

--- src/controller/user_controller.js
+++ src/controller/user_controller.js
@@ -1,4 +1,6 @@
-export const create_user = (req, res) => {
+import user_model from '../model/user_model.js'
+
+export const create_user = async (req, res) => {
     try {
         const data = req.body
 
@@ -6,20 +8,20 @@ export const create_user = (req, res) => {
 
         const nameRe = /^[A-Za-z ]{2,50}$/;
         const emailRe = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/;
-        const passRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        const passRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?& ]{8,}$/;
 
 
-        if(!name) return res.status(400).send({ status: false, msg: "name is required" })
-        if(name.trim().length == 0  || !nameRe.test(name.trim())) return res.status(400).send({ status: false, msg: "Invalid name" })
-`
+        if (!name) return res.status(400).send({ status: false, msg: "name is required" })
+        if (!nameRe.test(name)) return res.status(400).send({ status: false, msg: "Invalid name" })
+
         if (!email) return res.status(400).send({ status: false, msg: "email is required" })
-        if (email.trim().length == 0  || !emailRe.test(email.trim())) return res.status(400).send({ status: false, msg: "Invalid email" })
+        if (!emailRe.test(email.trim())) return res.status(400).send({ status: false, msg: "Invalid email" })
 
-        if (!password) return res.status(400).send({ status: false, msg: "password is required" })`
-        if (password.trim().length == 0  || !passRe.test(password.trim())) return res.status(400).send({ status: false, msg: "Invalid Password" })
+        if (!password) return res.status(400).send({ status: false, msg: "password is required" })
+        if (!passRe.test(password)) return res.status(400).send({ status: false, msg: "Invalid Password" })
 
-        console.log(data)
-        return res.send({ status: true, msg: "ok" })
+        const DB = await user_model.create(data)
+        return res.status(201).send({ status: true, msg: "SucessFull Create User", DB })
     }
     catch (err) { return res.status(500).send({ status: false, msg: err.message }) }
 } 
---
+++ src/model/user_model.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, trim: true, require: true },
+    email: { type: String, trim: true, require: true, unique: true },
+    password: { type: String, trim: true, require: true },
+})
+
+export default mongoose.model('users', userSchema)
